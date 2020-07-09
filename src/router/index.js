6c5b70dea77bdd321e84335345fbbb8d54b495d6
ASimpleMotion <pustovojt16@gmail.com>
2020-07-09 13:23:14 +0300

    Save prod config

--- src/router/index.js
+++ src/router/index.js
@@ -13,8 +13,8 @@ Vue.use(VueRouter)
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  //mode: 'history',
+  //base: process.env.BASE_URL,
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  //publicPath: '/CorsCheck/',
   "transpileDependencies": [
     "vuetify"
   ]
